258da4c622967ecbc448fe5002c19d531dac596c
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-23 14:35:24 +0900

    コントローラー開発_comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,11 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    # @commentがdestroyされる前に、commentが誰のものかを変数に保存する
+    customer_id = @comment.customer_id
+    @comment.destroy
+    # さっき保存したcustomer_idをここで使う
+    redirect_to customer_url(customer_id)
   end
 end
