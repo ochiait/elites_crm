4dc6f6f8bb3db4402c488b047392bc3412fc594f
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-23 13:59:19 +0900

    commentモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123005609) do
+ActiveRecord::Schema.define(version: 20151123045750) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "customer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["customer_id"], name: "index_comments_on_customer_id"
 
   create_table "customers", force: :cascade do |t|
     t.string   "family_name", null: false
