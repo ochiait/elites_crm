3f7925618c2d593ee1f5f00f391a868c255d2c58
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-23 21:19:51 +0900

    ログイン制限を緩和

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
      # @customers = Customer.page(params[:page])
