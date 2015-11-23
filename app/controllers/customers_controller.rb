a53c9656f9de30129561a3723e1051fe911ed201
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-23 15:49:19 +0900

    ransackの実装３

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,8 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.page(params[:page])
+     # @customers = Customer.page(params[:page])
+    @q = Customer.search(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
