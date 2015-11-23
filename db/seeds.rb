9eed7a1e824c92796b094d4dd3a40f778d1b5a48
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-23 15:17:28 +0900

    seedファイルを書き換える

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |index|
+  Customer.create(
+    family_name: Faker::Japanese::Name.last_name,
+    given_name: Faker::Japanese::Name.first_name,
+    email: "customer_#{index}@sparta.com"
+  )
+end
